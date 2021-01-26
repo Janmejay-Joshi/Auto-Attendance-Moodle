45684b880a6db5852d6f26a45dd7eb6079fceae7
Janmejay-Joshi <46400619+Janmejay-Joshi@users.noreply.github.com>
2021-01-26 19:51:18 +0530

    Add files via upload

---
+++ AtendanceMethod.py
@@ -0,0 +1,112 @@
+
+# Import WebScraping and Form Filling Libraries
+
+from bs4 import BeautifulSoup
+import requests
+from lxml import html
+
+"""
+    Attendance class defing and allocating prerequisits which are required before marking attendance
+"""
+
+class Attendance():
+    
+    def __init__(self, Lecture, Session):
+
+        self.lecture_link, self.Attendance_Type = self.Attendance_Link_Type(Lecture)
+        self.session = Session
+        self.Lecture = Lecture
+        
+        self.Assign_Attender()
+
+    def Find_Link(self):
+        result = self.session.get(self.lecture_link, headers = dict(referer = self.lecture_link))
+        soup = BeautifulSoup(result.content, 'html5lib')
+        Table = soup.find_all('table', attrs = {"class":"generaltable attwidth boxaligncenter"})[0]
+        Columns = Table.find_all('tr')
+        Submit = None
+
+        for Column in Columns:
+            Submit = Column.find('a')
+            if Submit != None:
+                break
+
+        if(Submit != None):
+            Attendance_Mark_link = Submit['href'];
+        else:
+            print("No Attendance Link Found...")
+            exit(0)
+
+        return Attendance_Mark_link
+
+    def Attendance_Link_Type(self, Lecture): 
+        switcher = { 
+            "EEES": ("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=1194",2), 
+            "LA": ("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=789",1), 
+            "AI": ("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=823",2),
+            "ICP":("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=816",2),
+            "ICPL":("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=1454",2),
+            "BEEE":("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=1088",2),
+            "BEEEL":("http://op2020.mitsgwalior.in/mod/attendance/view.php?id=2077",2)
+        } 
+
+        return switcher.get(Lecture, 0) 
+
+    def Assign_Attender(self):
+
+        attendance_link = self.Find_Link()
+        attendance_type = self.Attendance_Type
+        atender = Attender(attendance_link, self.lecture_link, self.session)
+
+        if attendance_type == 1:
+            print("Type1: Direct Link")
+            atender.direct_link()
+
+        elif attendance_type == 2:
+            print("Type2: Present Button")
+            atender.present_button()
+        else:
+            exit(0)
+
+"""
+    Attender class marks the actual atendance on basis of which type of Lecture it is
+"""
+
+class Attender():
+
+    def __init__(self, Attendance_Link, Lecture_Link, session):
+
+        self.attendance_link = Attendance_Link
+        self.lecture_link = Lecture_Link
+        self.session = session
+    
+    def direct_link(self):
+
+        attendance_page = self.session.get(self.attendance_link, headers = dict(referer = self.lecture_link), allow_redirects = True)
+
+        print(attendance_page) 
+        if str(attendance_page) == "<Response [200]>":
+            print("Attendance Marked... [^_^]")
+            exit(0)
+        else:
+            print("Unsuccessful")
+
+# currentlly not working present_button(self)
+
+    def present_button(self):
+
+        attendance_page = self.session.get(self.attendance_link, headers = dict(referer = self.lecture_link), allow_redirects = True)
+        print (self.attendance_link)
+        print(attendance_page) 
+        
+        payload = {
+                    "status":"625",
+                    "submitbutton":"Save changes"
+                }
+
+        result = self.session.post(self.attendance_link, data=payload, headers = dict(referer = self.attendance_link),allow_redirects=True)
+
+        print(result.content)
+                
+        
+
---
+++ AutoAtendance.py
@@ -0,0 +1,78 @@
+
+# load Required libraries and method
+
+import AtendanceMethod
+import requests
+from datetime import datetime,timedelta
+from csv import reader
+from lxml import html
+from bs4 import BeautifulSoup
+from os import path
+
+# Check if credentials exists if not create them else load them
+
+if not path.exists("./credentials"):
+    with open("./credentials",'w') as cred_file:
+        USERNAME = input("Enter Moodle Username: ")
+        PASSWORD = input("Enter Moodle Password: ")
+        
+        cred_file.write(f"{USERNAME}\n{PASSWORD}")
+else:
+    with open("./credentials",'r') as cred_file:
+        
+        cred = cred_file.readlines()
+        USERNAME = cred[0] 
+        PASSWORD = cred[1] 
+
+LOGIN_URL = "http://op2020.mitsgwalior.in/login/index.php" 
+
+
+def main():
+   
+        #Aporach Schedule
+        with open('Schedule.csv', encoding = "utf-8") as csvfile:
+            spamreader = reader(csvfile)
+            now = datetime.now()
+
+            for Schedule in spamreader:
+                if Schedule[0] == now.strftime("%A")[:3]: 
+
+                    schedule_time = datetime.strptime(Schedule[1],"%H:%M").replace(year=int(now.strftime("%Y")),month=int(now.strftime("%m")),day=int(now.strftime("%d"))) 
+                    if schedule_time < now and schedule_time+ timedelta(hours=1) > now:
+                        Lecture = Schedule[2]
+                else:
+                    print("\nNo Class Right now [<.>_<.>]\n")
+                    Lecture = "AI"
+                    exit(0)
+
+
+        print(f"\nLecture now is : {Lecture}")
+        print("Loging in...", end = "\r")
+
+        
+        # Setup session and cookies
+        session_requests = requests.session()
+
+        # Get login csrf token
+        result = session_requests.get(LOGIN_URL)
+        tree = html.fromstring(result.text)
+        authenticity_token = list(set(tree.xpath("//input[@name='logintoken']/@value")))[0]
+
+        # Create payload
+        payload = {
+            "username": USERNAME, 
+            "password": PASSWORD, 
+            "logintoken": authenticity_token
+        }
+
+        # Perform login
+        session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
+        print("Logged in...")
+
+        
+        # Mark Atendance
+
+        AtendanceMethod.Attendance(Lecture, session_requests)
+
+if __name__ == '__main__':
+    main()
